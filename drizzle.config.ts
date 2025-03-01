3a116e7f2c0c69a6ab95a10230580c9ad7d9567a
Седых Данила <sedyh@proitr.ru>
2025-03-01 13:33:11 +0300

    fix drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,7 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./drizzle",
-  driver: "pg",
-  dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
-  },
   verbose: true,
   strict: true,
+  dialect: "postgresql",
 });
